683a764f574489a6848be86eda99be791a731ca1
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-30 09:13:13 +0200

    working with routing part 2: router and history

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { User } from '../models/Model';
 import { AuthService } from '../services/AuthService';
 import { Login } from './Login';
-
+import {Router, Route, Switch} from 'react-router-dom';
+import history from '../utils/hostory';
+import { NavBar } from './NavBar';
+import { Home } from './Home';
+import { Profile } from './Profile';
 interface AppState{
   user:User |undefined;
 }
@@ -13,6 +17,9 @@ export class App extends React.Component<{},AppState>{
 constructor(props: any){
   super(props);
   this.setUser=this.setUser.bind(this);
+  this.state={
+    user:undefined
+  }
 }
 
   private setUser(user:User){
@@ -26,8 +33,19 @@ constructor(props: any){
   render(){
     return (
       
-      <div>App Works using class
-        <Login authService={this.authService} setUser={this.setUser}/>
+      <div className="wrapper">
+        <Router history={history} >
+          <div>
+            <NavBar user={this.state.user}/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/login'>
+                <Login authService={this.authService} setUser={this.setUser}/>
+              </Route>
+              <Route exact path='/profile' component={Profile}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
