05c7c1998e96166c0e4b2d468d7e7ec0a6c90d34
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-30 17:40:40 +0200

    working with modal component

---
+++ src/components/spaces/ConfromModalComponent.tsx
@@ -0,0 +1,25 @@
+import { Component } from "react"
+
+interface ConfromModalComponentProps {
+    show: boolean,
+    content: string,
+    close: () => void
+}
+
+export class ConfromModalComponent extends Component<ConfromModalComponentProps> {
+
+    render() {
+        if (!this.props.show) {
+            return null
+        } else {
+            return <div className='modal'>
+                <div className='modalContent'>
+                    <h2>You tried to resrve and ...</h2>
+                    <h3 className='modalText'>{this.props.content}</h3>
+                    <button  onClick={()=> this.props.close()} >Ok, close</button>
+                </div>
+            </div>
+        }
+    }
+
+} 
